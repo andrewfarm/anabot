ff2bcfad488ac23ec6c15ed79898d820e7c66996
andrewfarm <Andrew.Farm@tufts.edu>
2017-11-21 17:15:29 -0500

    Optimize markov chain builder

--- mark.py
+++ mark.py
@@ -1,28 +1,68 @@
 import re
 import json
 
-def uniq(s):
+def sortedIndex(list, elem, startIndex=None, endIndex=None):
+        if startIndex is None:
+                startIndex = 0
+        if endIndex is None:
+                endIndex = len(list) - 1
+        if endIndex < startIndex:
+                return -1
+        pivotIndex = int((startIndex + endIndex) / 2)
+        if elem == list[pivotIndex]:
+                return pivotIndex
+        elif elem < list[pivotIndex]:
+                return sortedIndex(list, elem, startIndex, pivotIndex - 1)
+        else:
+                return sortedIndex(list, elem, pivotIndex + 1, endIndex)
+
+def insertIntoSorted(list, elem, startIndex=None, endIndex=None):
+        if startIndex is None:
+                startIndex = 0
+        if endIndex is None:
+                endIndex = len(list) - 1
+        if endIndex < startIndex:
+                list.insert(elem, startIndex)
+        else:
+                pivotIndex = int((startIndex + endIndex) / 2)
+                if elem == list[pivotIndex]:
+                        list.insert(elem, pivotIndex + 1)
+                elif elem < list[pivotIndex]:
+                        insertIntoSorted(list, elem, startIndex, pivotIndex - 1)
+                else:
+                        insertIntoSorted(list, elem, pivotIndex + 1, endIndex)
+
+def uniq(sortedList):
         u = []
-        for elem in s:
-                if not elem in u:
+        curr = None
+        for elem in sortedList:
+                if elem != curr:
+                        curr = elem
                         u.append(elem)
         return u
 
 def buildMarkov(text):
         print 'Splitting text'
-        textAsSymbols = re.split(r'\s+', text)
+        textAsSymbols = re.split(r'\s+', text.lower())
+        print 'Sorting symbols'
+        sortedText = sorted(textAsSymbols)
         print 'Building unique symbol list'
-        symbols = uniq(textAsSymbols)
+        symbols = uniq(sortedText)
+        
         print 'Building successive symbol list'
-        print 'This may take a while...'
+        
+        #create list of empty lists
         strings = []
         for i in xrange(len(symbols)):
                 strings.append([])
+        #populate successive symbol list
         for i in xrange(len(textAsSymbols) - 1):
-                symbolIndex = symbols.index(textAsSymbols[i])
+                symbolIndex = sortedIndex(symbols, textAsSymbols[i])
                 strings[symbolIndex].append(textAsSymbols[i+1])
+
         print 'Done'
-        return strings
+        markov = {'symbols': symbols, 'next': strings}
+        return markov
 
 inFilename = 'gutenberg-mobydick.txt'
 inf = open(inFilename, 'r')
